9d315ed68e870f4ed1c585c469eb694375cee9c0
ronhahaha <kidskidd_ing@hotmail.com>
2020-03-25 00:45:24 +0800

    static files for demo

--- src/api/table.js
+++
@@ -1,9 +0,0 @@
-import request from '@/utils/request'
-
-export function fetchMembers(query) {
-  return request({
-    url: 'https://randomuser.me/api/?results=20',
-    method: 'get',
-    params: query
-  })
-}
